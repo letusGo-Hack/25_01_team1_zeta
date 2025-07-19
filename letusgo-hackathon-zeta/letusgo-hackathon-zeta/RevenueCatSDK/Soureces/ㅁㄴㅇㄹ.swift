585e9d1aa00960a46ffa4ae196324805094ecd0a
이건우 <lgw9898@naver.com>
2025-07-19 12:35:33 +0900

    상품 구매 로직 구현

--- letusgo-hackathon-zeta/letusgo-hackathon-zeta/RevenueCatSDK/Soureces/ㅁㄴㅇㄹ.swift
+++ letusgo-hackathon-zeta/letusgo-hackathon-zeta/RevenueCatSDK/Soureces/ㅁㄴㅇㄹ.swift
@@ -11,25 +11,116 @@ import SwiftUI
 import StoreKit
 import Combine
 
+enum InAppPurchaseFeature {
+    
+    /// 상품
+    enum Product: CaseIterable {
+        case weekly
+        case monthly
+        case weekly2
+        case nonConsumable
+        case consumable
+        
+        /// 상품 아이디
+        var productID: String {
+            switch self {
+            case .weekly:
+                return "maestro.weekly.tests.01"
+            case .monthly:
+                return "maestro.monthly.tests.02"
+            case .weekly2:
+                return "maestro.weekly2.tests.01"
+            case .nonConsumable:
+                return "maestro.nonconsumable.tests.01"
+            case .consumable:
+                return "maestro.consumable.tests.01"
+            }
+        }
+        
+        /// UI에 표시되는 이름
+        var displayName: String {
+            switch self {
+            case .weekly: return "주간 구독"
+            case .monthly: return "월간 구독"
+            case .weekly2: return "주간 구독2"
+            case .nonConsumable: return "영구 상품"
+            case .consumable: return "소모성 상품"
+            }
+        }
+    }
+}
+
 final class InAppPurchaseViewModel: ObservableObject {
     
+    /// 상품정보
+    @Published var products = InAppPurchaseFeature.Product.allCases
+    
     enum Action {
+        case view(ViewAction)
         
+        enum ViewAction {
+            case buyButtonTapped
+        }
     }
     
     func send(action: Action) {
-        
+        switch action {
+        case let .view(viewAction):
+            switch viewAction {
+            case .buyButtonTapped:
+                handleBuyButtonTapped()
+            }
+        }
+    }
+    
+    private func handleBuyButtonTapped() {
+        Task {
+            let product = InAppPurchaseFeature.Product.nonConsumable.productID
+            let fetchedProducts = await Purchases.shared.products([product])
+            
+            guard let product = fetchedProducts.first else {
+                // handle error
+                return
+            }
+            
+            do {
+                _ = try await Purchases.shared.purchase(product: product)
+            } catch {
+                // handle error
+            }
+        }
     }
 }
 
-public struct InAppPurchaseView {
+struct InAppPurchaseView: View {
     @StateObject private var viewModel = InAppPurchaseViewModel()
     
     var body: some View {
-        Text("Hello, World!")
-        
-        
+        VStack {
+            
+            Spacer()
+            
+            BuyButton
+        }
+        .navigationTitle("인앱결제")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    /// 인앱 결제 버튼
+    private var BuyButton: some View {
+        Button {
+            viewModel.send(action: .view(.buyButtonTapped))
+        } label: {
+            Text("구매하기")
+        }
+        .padding()
+        .containerRelativeFrame(.horizontal)
+        .glassEffect()
     }
 }
 
-
+#Preview {
+    NavigationStack {
+        InAppPurchaseView()
+    }
+}
