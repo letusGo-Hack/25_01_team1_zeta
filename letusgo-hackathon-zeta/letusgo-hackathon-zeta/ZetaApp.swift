e66060979a19799702adeda7549ef1157808a5ff
sangjin <sangjincha719@gmail.com>
2025-07-19 11:58:51 +0900

    채팅 UI 1차 구성

--- letusgo-hackathon-zeta/letusgo-hackathon-zeta/ZetaApp.swift
+++ letusgo-hackathon-zeta/letusgo-hackathon-zeta/ZetaApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct ZetaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            ChatFeature()
         }
     }
 }
